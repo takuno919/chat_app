ce94ab95ff0ab03522a6c489c73020750e3338e4
Nishimura <takuno919@gmail.com>
2017-06-13 16:11:47 +0900

    finished step 3

---
+++ app/assets/javascripts/src/actions/messages.js
@@ -0,0 +1,20 @@
+// actions/messages.js
+import Dispatcher from '../dispatcher'
+
+export default {
+  changeOpenChat(newUserID) {
+    Dispatcher.handleViewAction({
+      type: 'updateOpenChatID',
+      userID: newUserID,
+    })
+  },
+  // 追記
+  sendMessage(userID, message) {
+    Dispatcher.handleViewAction({
+      type: 'sendMessage',
+      userID: userID,
+      message: message,
+      timestamp: +new Date(),
+    })
+  },
+}
--- app/assets/javascripts/src/components/messages/replyBox.js
+++ app/assets/javascripts/src/components/messages/replyBox.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import MessagesStore from '../../stores/messages'  // 追記
+import MessagesAction from '../../actions/messages' // 追記
 
 class ReplyBox extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.initialState
+  }
+
+  get initialState() {
+    return {
+      value: '',
+    }
+  }
+
+  handleKeyDown(e) {
+    if (e.keyCode === 13) {
+      MessagesAction.sendMessage(MessagesStore.getOpenChatUserID(), this.state.value)
+      this.setState({
+        value: '',
+      })
+    }
+  }
+
+  updateValue(e) {
+    this.setState({
+          value: e.target.value,
+    })
+  }
 
   render() {
     return (
       <div className='reply-box'>
         <input
+          value={ this.state.value }  // 追記
+          onKeyDown={ this.handleKeyDown.bind(this) } // 追記
+          onChange={ this.updateValue.bind(this) }
           className='reply-box__input'
           placeholder='Type message to reply..'
         />
